48b9af527b1fc2759455c8f75da85d09f0601528
franciscomororo1 <junior.mororo@gmail.com>
2025-12-16 23:31:01 +0000

    Continuando projeto Django

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+
+from blog.forms import PostForm
 from .models import Post
 from django.utils import timezone
 
@@ -8,4 +10,16 @@ def post_list(request):
 
 def post_detail(request, pk):
     post= get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", )
+    return render(request, "blog/post_detail.html", {'post' :post})
+
+def post_new(request):
+    if request.method =="Post":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
